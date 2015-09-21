3bbe04bf52ed2062d31439a014783a3cc928f40b
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-09-21 01:49:16 -0400

    Updating testbench for the ripple carry adder

--- hw1/q1/tb_adder_rc_8.v
+++ hw1/q1/tb_adder_rc_8.v
@@ -6,37 +6,46 @@
 
 module tb_adder_rc_8;
 
-reg [7:0] A, B;
-wire [8:0] S;
+reg [ 7: 0 ] A, B;
+wire [ 8: 0 ] S;
 
+// Indicates a gate delay cycle on every rising edge
+reg delay_clock;
 
 adder_rc_8 DUT (
-        .A( A ),
-        .B( B ),
-        .S( S )
-      );
+             .A( A ),
+             .B( B ),
+             .S( S )
+           );
 
 initial
 begin
   // Save signal simulation data
   $dumpfile( "adder_rc_8_propagation.vcd" );
-  $dumpvars( 1, A, B, S );
+  $dumpvars( 1, delay_clock, A, B, S );
   $dumpflush;
+
+  delay_clock = 1'b0;
 
   A = 8'b0;
   B = 8'b0;
 
   // Propagate initial state
-  #200
+  #200;
 
   A = 8'b1111_1111;
 
   // Need to wait for the propagation of data through the A path
-  #200
+  #200;
 
   B = 8'b1;
 
   #500 $stop;
 end
+
+always forever
+  begin
+    delay_clock <= #5 ~delay_clock;
+  end
 
 endmodule
