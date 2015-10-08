e9c739d26d7eac9d410407e3e94c3162b1a1cb50
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-10-08 18:45:22 -0400

    Name the module instances in the SSP top level

--- hw2/q1/ssp.v
+++ hw2/q1/ssp.v
@@ -19,50 +19,51 @@ wire RxNextWord, TxValidWord, TxNextWord, TxIsEmpty;
 wire [ 7: 0 ] RxData, TxData;
 
 // Instance rx_fifo
-rx_fifo
-  (
-    .PCLK( PCLK ),
-    .CLEAR_B( CLEAR_B ),
-    .PSEL( PSEL ),
-    .PWRITE( PWRITE ),
-    .RxData( RxData ),
-    .NextWord( RxNextWord ),
-    .PRDATA( PRDATA ),
-    .SSPRXINTR( SSPRXINTR )
-  );
+rx_fifo rx
+        (
+          .PCLK( PCLK ),
+          .CLEAR_B( CLEAR_B ),
+          .PSEL( PSEL ),
+          .PWRITE( PWRITE ),
+          .RxData( RxData ),
+          .NextWord( RxNextWord ),
+          .PRDATA( PRDATA ),
+          .SSPRXINTR( SSPRXINTR )
+        );
 
 // Instance tx_fifo
-tx_fifo
-  (
-    .PCLK( PCLK ),
-    .CLEAR_B( CLEAR_B ),
-    .PSEL( PSEL ),
-    .PWRITE( PWRITE ),
-    .PWDATA( PWDATA ),
-    .NextWord( TxNextWord ),
-    .ValidWord( TxValidWord ),
-    .IsEmpty( TxIsEmpty ),
-    .TxData( TxData ),
-    .SSPTXINTR( SSPTXINTR )
-  );
+tx_fifo tx
+        (
+          .PCLK( PCLK ),
+          .CLEAR_B( CLEAR_B ),
+          .PSEL( PSEL ),
+          .PWRITE( PWRITE ),
+          .PWDATA( PWDATA ),
+          .NextWord( TxNextWord ),
+          .ValidWord( TxValidWord ),
+          .IsEmpty( TxIsEmpty ),
+          .TxData( TxData ),
+          .SSPTXINTR( SSPTXINTR )
+        );
 
 // Instance ssp_tx_rx
-ssp_tx_rx (
-    .PCLK( PCLK ),
-    .CLEAR_B( CLEAR_B ),
-    .SSPCLKIN( SSPCLKIN ),
-    .SSPFSSIN( SSPFSSIN ),
-    .SSPRXD( SSPRXD ),
-    .TxData( TxData ),
-    .TxValidWord( TxValidWord ),
-    .TxNextWord( TxNextWord ),
-    .TxIsEmpty( TxIsEmpty ),
-    .RxData( RxData ),
-    .RxNextWord( RxNextWord ),
-    .SSPCLKOUT( SSPCLKOUT ),
-    .SSPFSSOUT( SSPFSSOUT ),
-    .SSPTXD( SSPTXD ),
-    .SSPOE_B( SSPOE_B )
-  );
+ssp_tx_rx logic
+          (
+            .PCLK( PCLK ),
+            .CLEAR_B( CLEAR_B ),
+            .SSPCLKIN( SSPCLKIN ),
+            .SSPFSSIN( SSPFSSIN ),
+            .SSPRXD( SSPRXD ),
+            .TxData( TxData ),
+            .TxValidWord( TxValidWord ),
+            .TxNextWord( TxNextWord ),
+            .TxIsEmpty( TxIsEmpty ),
+            .RxData( RxData ),
+            .RxNextWord( RxNextWord ),
+            .SSPCLKOUT( SSPCLKOUT ),
+            .SSPFSSOUT( SSPFSSOUT ),
+            .SSPTXD( SSPTXD ),
+            .SSPOE_B( SSPOE_B )
+          );
 
 endmodule
