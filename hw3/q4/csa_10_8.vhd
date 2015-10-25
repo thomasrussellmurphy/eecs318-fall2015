41d86fefc1deae0c4ce1de16c0500625c9aad1d5
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-10-24 22:32:56 -0400

    Initial completion of the csa_10_8 implementation

--- hw3/q4/csa_10_8.vhd
+++ hw3/q4/csa_10_8.vhd
@@ -1,6 +1,7 @@
 -- Thomas Russell Murphy (trm70)
 -- EECS 318 Fall 2015
--- Generic width ripple-carry adder
+-- Carry-Save Adder for ten 8-bit inputs
+-- Maximum output value is about 11.3 bits, requiring 12 bit output width
 
 library IEEE;
   use IEEE.STD_LOGIC_1164.ALL;
@@ -39,6 +40,30 @@ architecture RTL of csa_10_8 is
     s_ghi_w, c_ghi_w,
     s_csa2_a, c_csa2_a,
     s_csa2_b, c_csa2_b : std_logic_vector(8 downto 0);
+
+  -- For Layer 3
+  signal
+    s_csa2_a_w, c_csa2_a_w,
+    s_csa2_b_w, c_csa2_b_w,
+    j_w,
+    s_csa3_a, c_csa3_a,
+    s_csa3_b, c_csa3_b : std_logic_vector(9 downto 0);
+
+  -- For Layer 4
+  signal
+    s_csa3_a_w, c_csa3_a_w,
+    s_csa3_b_w,
+    s_csa4, c_csa4 : std_logic_vector(10 downto 0);
+
+  -- For Layer 5
+  signal
+    c_csa3_b_w,
+    s_csa4_w, c_csa4_w,
+    s_csa5, c_csa5 : std_logic_vector(11 downto 0);
+
+  -- For Final Adder
+  signal s_csa5_w, c_csa5_w : std_logic_vector(12 downto 0);
+  signal wide_result : std_logic_vector(13 downto 0);
 
   -- Components
   component csa is
@@ -115,12 +140,72 @@ begin
   -- Layer 3: consumes s_csa2_A, c_csa2_A, s_csa2_B, c_csa2_B, j
   -- Widths are now 10
 
+  s_csa2_a_w <= '0' & s_csa2_a;
+  c_csa2_a_w <= c_csa2_a & '0';
+  s_csa2_b_w <= '0' & s_csa2_b;
+  c_csa2_b_w <= c_csa2_b & '0';
+  j_w <= B"00" & j;
+
+  layer_3: for index in 0 to 9 generate
+    csa3_a: csa port map (
+      x => s_csa2_a_w(index),
+      y => c_csa2_a_w(index),
+      z => s_csa2_b_w(index),
+      c => c_csa3_a(index),
+      s => s_csa3_a(index) );
+
+    csa3_b: csa port map (
+      x => c_csa2_b_w(index),
+      y => j_w(index),
+      z => '0',
+      c => c_csa3_b(index),
+      s => s_csa3_b(index) );
+  end generate layer_3;
+
   -- Layer 4: consumes s_csa3_A, c_csa3_A, s_csa3_B
   -- Widths are now 11
+
+  s_csa3_a_w <= '0' & s_csa3_a;
+  c_csa3_a_w <= c_csa3_a & '0';
+  s_csa3_b_w <= '0' & s_csa3_b;
+
+  layer_4: for index in 0 to 10 generate
+    csa4: csa port map (
+      x => s_csa3_a_w(index),
+      y => c_csa3_a_w(index),
+      z => s_csa3_b_w(index),
+      c => c_csa4(index),
+      s => s_csa4(index) );
+  end generate layer_4;
 
   -- Layer 5: consumes s_csa4, c_csa4, c_csa3_B
   -- Widths are now 12
 
+  c_csa3_b_w <= '0' & c_csa3_b & '0';
+  s_csa4_w <= '0' & s_csa4;
+  c_csa4_w <= c_csa4 & '0';
+
+  layer_5: for index in 0 to 11 generate
+    csa5: csa port map (
+      x => c_csa3_b_w(index),
+      y => s_csa4_w(index),
+      z => c_csa4_w(index),
+      c => c_csa5(index),
+      s => s_csa5(index) );
+  end generate layer_5;
+
   -- Final adder
   -- Widths are now 13
+
+  s_csa5_w <= '0' & s_csa5;
+  c_csa5_w <= c_csa5 & '0';
+
+  final_adder: adder_rc generic map (
+      width => 13 )
+    port map (
+      a => s_csa5_w,
+      b => c_csa5_w,
+      s => wide_result );
+
+  z <= wide_result(11 downto 0);
 end RTL;
