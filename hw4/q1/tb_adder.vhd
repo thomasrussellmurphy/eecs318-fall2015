b4c4f9758b78078bf9f7bb4c800a0c8067bc1ac3
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-11-11 12:08:03 -0500

    Lots of copy/paste to complete the tb_adder

--- hw4/q1/tb_adder.vhd
+++ hw4/q1/tb_adder.vhd
@@ -99,11 +99,87 @@ begin
     wait until rising_edge(test_clk);
 
     -- Operations with unsigned addition, code "001"
-    -- Operations with signed subtraction
-    -- Operations with unsigned subtraction
-    -- Operations with signed increment
-    -- Operations with signed decrement
+    set_inputs(x"0000", x"0001", "001", '0', '0');
+    wait until rising_edge(test_clk);
 
+    set_inputs(x"000F", x"000F", "001", '1', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"7F00", x"0300", "001", '0', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"FF00", x"0100", "001", '1', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"8100", x"8000", "001", '1', '1');
+    wait until rising_edge(test_clk);
+
+    -- Operations with signed subtraction, code "010"
+    set_inputs(x"0000", x"0001", "010", '0', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"000F", x"000F", "010", '1', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"7F00", x"0300", "010", '0', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"FF00", x"0100", "010", '1', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"8100", x"8000", "010", '1', '1');
+    wait until rising_edge(test_clk);
+
+    -- Operations with unsigned subtraction, code "011"
+    set_inputs(x"0000", x"0001", "011", '0', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"000F", x"000F", "011", '1', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"7F00", x"0300", "011", '0', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"FF00", x"0100", "011", '1', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"8100", x"8000", "011", '1', '1');
+    wait until rising_edge(test_clk);
+
+    -- Operations with signed increment
+    set_inputs(x"0000", x"0001", "100", '0', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"000F", x"000F", "100", '1', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"7F00", x"0300", "100", '0', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"FF00", x"0100", "100", '1', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"8100", x"8000", "100", '1', '1');
+    wait until rising_edge(test_clk);
+
+    -- Operations with signed decrement, code "101"
+    set_inputs(x"0000", x"0001", "101", '0', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"000F", x"000F", "101", '1', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"7F00", x"0300", "101", '0', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"FF00", x"0100", "101", '1', '0');
+    wait until rising_edge(test_clk);
+
+    set_inputs(x"8100", x"8000", "101", '1', '1');
+    wait until rising_edge(test_clk);
+
+    -- Final cycle of wait with input reset
+    set_inputs(x"0000", x"0000", "000", '0', '0');
     wait until rising_edge(test_clk);
 
     end_simulation <= true;
