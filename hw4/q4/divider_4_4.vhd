cfc1828f1c910661678c14e61f05816287a5abb5
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-11-12 10:30:10 -0500

    Attempted implementation of algorithm confusingly described in assignment

--- hw4/q4/divider_4_4.vhd
+++ hw4/q4/divider_4_4.vhd
@@ -14,12 +14,50 @@ entity divider_4_4 is
 end entity divider_4_4;
 
 architecture RTL of divider_4_4 is
-  -- Signals
 begin
 
   division: process(a, b) is
+    variable
+      p, p_p, d : signed(7 downto 0) := (others => '0');
+    variable q : signed(3 downto 0) := (others => '0');
   begin
-    z <= a / b;
+    -- p ~ A; d ~ Q; b ~ M;
+
+    p := resize(a, p'length);
+    d := resize(b, d'length);
+
+    for i in 3 downto 0 loop
+      p := shift_left(p, 1);
+      d := shift_left(d, 1);
+
+      -- Retain previous
+      p_p := p;
+
+      if p(7) = b(3) then
+        -- Same sign
+        p := p - b;
+      else
+        -- Different sign
+        p := p + b;
+      end if;
+
+      if (p(7) = p_p(7)) or (p = 0) then
+        -- Success
+        d(0) := '1';
+      else
+        -- Failure, with restore
+        d(0) := '0';
+        p := p_p;
+      end if;
+    end loop;
+
+    q := d(7) & d(2 downto 0);
+    -- Check sign for final output
+    if a(3) = b(3) then
+      z <= q;
+    else
+      z <= - q;
+    end if;
   end process division;
 
 end RTL;
